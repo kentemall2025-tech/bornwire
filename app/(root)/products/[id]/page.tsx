2d81e2a95b8326faf3e59594cf3469bc56cc20dc
Marcus <owaremarcuskofi@gmail.com>
2025-11-25 20:39:56 +0000

    finish the chat icon

--- app/(root)/products/[id]/page.tsx
+++ app/(root)/products/[id]/page.tsx
@@ -1,35 +1,7 @@
-import { supabase } from "@/lib/supabase/supabase";
 import React from "react";
 
-export default async function Page() {
-  try {
-    const { data, error } = await supabase
-      .from("users")
-      .insert({
-        full_name: "Marcus Oware",
-        avatar: "",
-      })
-      .single();
+interface pageprops {}
 
-    if (error) {
-      console.error("Error inserting user:", error.message);
-      return (
-        <div>
-          <h2>Error inserting user: {error.message}</h2>
-        </div>
-      );
-    }
-
-    console.log("Inserted user:", data);
-
-    return (
-      <div>
-        <h2>User inserted successfully!</h2>
-        <p>{JSON.stringify(data)}</p>
-      </div>
-    );
-  } catch (error) {
-    console.error("Error:", error);
-    return <div></div>;
-  }
+export default function page() {
+  return <div className="h-full w-full"></div>;
 }
--- components/ui/chaticon.tsx
+++ components/ui/chaticon.tsx
@@ -13,11 +13,11 @@ export const ChatIcon = () => {
   return (
     <Link
       href={"/chat"}
-      className="fixed -bottom-10 right-6  text-white shadow-lg hover:bg-blue-400 text-3xl z-50 rounded-lg bg-blue-500 "
+      className="fixed bottom-4 right-5  bg-yellow-500 bg-gradient-to-r from-orange-500  text-white shadow-lg hover:bg-blue-400 text-3xl z-50 rounded-lg bg-blue-500 "
     >
-      <Button className="">
-        <MessageCircle size={540} />
-      </Button>
+      <div className="w-20 h-20 object-contain p-4 ">
+        <MessageCircle className="object-contain" size={50} />
+      </div>
     </Link>
   );
 };
