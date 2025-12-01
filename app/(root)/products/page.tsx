e5a1c9220fc8d436ed86ef7376560f89a780d811
Marcus <owaremarcuskofi@gmail.com>
2025-12-01 01:01:53 +0000

    flowed patterns

--- app/(root)/products/page.tsx
+++ app/(root)/products/page.tsx
@@ -11,8 +11,6 @@ export default async function page() {
     .select("*")
     .single();
 
-  console.log(product);
-
   return (
     <section className="">
       <SearchBar />
--- components/ui/verticalproducts.tsx
+++ components/ui/verticalproducts.tsx
@@ -1,11 +1,14 @@
 import { horizonproductsContent } from "@/lib/constants";
 import VerticalProductCard from "./veticalproductcard";
+import { supabase } from "@/lib/supabase/supabase";
 
-export function VerticalProductList() {
+export async function VerticalProductList() {
+  let { data: products, error } = await supabase.from("product").select("*");
+
   return (
     <div className="w-full p-4 max-w-full ">
       <div className="grid  w-full grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 items-center ">
-        {horizonproductsContent.map((item, index) => (
+        {products?.map((item: any, index: number) => (
           <VerticalProductCard
             key={index}
             imageurl={item.imageurl}
