1bb65c17ecbf0a923358a87392ce6f244f8af801
Marcus <owaremarcuskofi@gmail.com>
2025-12-02 23:23:24 +0000

    added the logics for the chat app

--- app/(root)/chat/page.tsx
+++
@@ -1,9 +0,0 @@
-import RealtimeChat from "@/components/realtime-chat";
-
-export default function page() {
-  return (
-    <div className="w-full h-screen">
-      <RealtimeChat roomName="discussion" username="marcus" />
-    </div>
-  );
-}
