890943816a83e4cfdb0d2e4c47fc1b883ff9f859
Marcus <owaremarcuskofi@gmail.com>
2025-12-02 21:27:58 +0000

    added the logics for the chat app

--- lib/constants.ts
+++ lib/constants.ts
@@ -18,7 +18,7 @@ export const showcaseContent = [
   {
     id: "4",
     imageurl:
-      "https://csmvkgdme8w3hyot.public.blob.vercel-storage.com/WhatsApp Image 2025-11-22 at 9.36.48 PM.jpeg",
+      "https://csmvkgdme8w3hyot.public.blob.vercel-storage.com/ddf60025-df47-406f-83e4-355b36924db5.jpg",
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore, magnam ipsam? Illum, iste et. Aliquid, animi aspernatur? Quisquam, dolorem eveniet.",
   },
