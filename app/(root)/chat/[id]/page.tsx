b045d0e83476a8264208d47c05b6b751a412cd09
Marcus <owaremarcuskofi@gmail.com>
2025-12-03 00:14:50 +0000

    added the logics for the chat app

--- app/(root)/chat/[id]/page.tsx
+++ app/(root)/chat/[id]/page.tsx
@@ -11,7 +11,7 @@ export default async function Page({
   const { roomName, username } = await searchParams;
 
   return (
-    <div className="max-w-[80%] mx-auto mt-5">
+    <div className="max-w-[80%] mx-auto mt-5 bg-yellow-500">
       <RealtimeChat
         username={username as string}
         roomName={roomName as string}
