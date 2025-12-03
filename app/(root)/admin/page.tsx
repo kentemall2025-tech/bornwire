6b638d8d4e3591fd46b3a60bf9258f66e7eeb1ad
Marcus <owaremarcuskofi@gmail.com>
2025-12-03 02:13:00 +0000

    dofjaio

--- app/(root)/admin/page.tsx
+++ app/(root)/admin/page.tsx
@@ -1,5 +1,8 @@
+import { supabase } from "@/lib/supabase/supabase";
 import React from "react";
 
-export default function page() {
+export default async function page() {
+  const { data } = await supabase.from("rooms").select("*");
+  console.log(data);
   return <div></div>;
 }
