c1c44948118f4771b292e0f814a601ccf3ddd735
Marcus <owaremarcuskofi@gmail.com>
2025-12-06 16:53:07 +0000

    father open our us

--- components/adminviewcard.tsx
+++ components/adminviewcard.tsx
@@ -127,7 +127,7 @@ export default function AdminRoomChat({ roomId }: { roomId: string }) {
   };
 
   return (
-    <div className="flex flex-col h-[50%] p-4">
+    <div className="flex flex-col h-[50%] p-4 bg-gray-500">
       {/* CHAT */}
       <div className="flex-1 overflow-y-auto space-y-3 pr-2 h-[40vh]">
         {messages.map((msg) => (
