48b8e3d8c11e1b4372b7257d5312018cf1087ff5
Marcus <owaremarcuskofi@gmail.com>
2025-12-06 00:59:51 +0000

    feat

--- components/realtime-chat.tsx
+++ components/realtime-chat.tsx
@@ -89,7 +89,7 @@ export default function RealTimeChat({ roomId }: any) {
     );
 
   return (
-    <div className="flex flex-col h-full">
+    <div className="flex flex-col h-screen">
       <div className="flex-1 overflow-y-auto p-4 space-y-3 bg-orange-50 h-[60vh]">
         {messages.map((msg: any) => (
           <div
