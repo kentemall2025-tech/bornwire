2b022bc928a0fbf9612aa0f60190a07ea5b47928
Marcus <owaremarcuskofi@gmail.com>
2025-12-06 14:23:50 +0000

    father open our us

--- app/(root)/admin/rooms/[id]/page.tsx
+++ app/(root)/admin/rooms/[id]/page.tsx
@@ -2,7 +2,7 @@ import AdminRoomChat from "@/components/adminviewcard";
 
 export default function Page({ params }: { params: { id: string } }) {
   return (
-    <div className="h-screen">
+    <div className="h-screen max-w-[80%] mx-auto">
       <AdminRoomChat roomId={params.id} />
     </div>
   );
