985dd16efb54d9a7b2366f50725efd0023f1c36a
Marcus <owaremarcuskofi@gmail.com>
2025-12-06 02:18:14 +0000

    fixed the damn thing

--- app/(root)/chat/[id]/page.tsx
+++ app/(root)/chat/[id]/page.tsx
@@ -5,7 +5,7 @@ import RealtimeChat from "@/components/realtime-chat";
 export default function AdminChatPage({ params }: any) {
   return (
     <div className="max-w-[80%] mx-auto mt-10">
-      <RealtimeChat roomId={id} />
+      <RealtimeChat roomId={params.id} />
     </div>
   );
 }
