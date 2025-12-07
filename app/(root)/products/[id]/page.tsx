f8d5f4c6b7350ba6897d900bc1949fb8050574cb
Marcus <owaremarcuskofi@gmail.com>
2025-12-07 07:07:46 +0000

    added the link

--- app/(root)/products/[id]/page.tsx
+++ app/(root)/products/[id]/page.tsx
@@ -1,5 +1,30 @@
+"use client";
+import { supabase } from "@/lib/supabase/supabase";
+import Image from "next/image";
 import React from "react";
 
-export default function page() {
-  return <div></div>;
+export default async function page({
+  params,
+}: {
+  params: {
+    id: string;
+  };
+}) {
+  const { id } = await params;
+  const { data: product, error } = await supabase
+    .from("products")
+    .select("*")
+    .eq("label", id)
+    .maybeSingle();
+  console.log(product);
+
+  return (
+    <div>
+      <div>
+        <div>
+          <Image src={""} alt={""} width={500} height={500} />
+        </div>
+      </div>
+    </div>
+  );
 }
